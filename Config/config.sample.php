472448a92dd631afc1486e5ea12a5053d4683b26
Pattricia Ruth Das <p.das.11@student.scu.edu.au>
2025-09-22 21:52:31 +0800

    config file added

---
+++ Config/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+// config/config.sample.php
+// Example configuration - edit and rename to config.php
+
+define('DB_HOST', 'localhost');   
+define('DB_USER', 'root'); 
+define('DB_PASS', ''); 
+define('DB_NAME', 'unilib');       
+
+// Example: $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+?>
